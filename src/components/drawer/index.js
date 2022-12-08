1ff5b8d06f3eb5ea6111607cde0d6345549693c2
Rahul Gupta <rahulr09gupta@gmail.com>
2022-12-08 13:04:11 +0530

    fix(drawer): remove warning by using transient prop
    substituted $isOpen for isOpen to remove unknown prop warning

--- src/components/drawer/index.js
+++ src/components/drawer/index.js
@@ -14,8 +14,8 @@ const Background = styled.div`
     top: 0;
     left: 0;
     background-color: #000;
-    opacity: ${(p) => (p.isOpen ? 0.5 : 0)};
-    visibility: ${(p) => (p.isOpen ? "unset" : "hidden")};
+    opacity: ${(p) => (p.$isOpen ? 0.5 : 0)};
+    visibility: ${(p) => (p.$isOpen ? "unset" : "hidden")};
     transition: all 0.3s;
 `;
 
@@ -28,7 +28,7 @@ const DrawArea = styled.div`
     right: 0;
     top: 0;
     z-index: 5;
-    transform: ${(p) => (p.isOpen ? "translateX(0%)" : "translateX(100%)")};
+    transform: ${(p) => (p.$isOpen ? "translateX(0%)" : "translateX(100%)")};
     transition: transform 0.3s ease-in-out;
     color: #c7c9ca;
     max-height: 100vh;
@@ -77,14 +77,14 @@ const DrawerToggle = styled.div`
     top: 50%;
     border-radius: 50% 0 0 50%;
     padding: 0.3rem;
-    box-shadow: ${(p) => (p.isOpen ? "none" : "0px 2px 2px 1px rgba(0, 0, 0, 0.5)")};
+    box-shadow: ${(p) => (p.$isOpen ? "none" : "0px 2px 2px 1px rgba(0, 0, 0, 0.5)")};
     cursor: pointer;
 `;
 
 const DrawerIcon = styled(RiArrowLeftSLine)`
     color: #fff;
     font-size: 2rem;
-    transform: ${(p) => (p.isOpen ? "rotate(180deg)" : "rotate(0deg)")};
+    transform: ${(p) => (p.$isOpen ? "rotate(180deg)" : "rotate(0deg)")};
     transition: transform 0.2s ease-in-out;
 `;
 
@@ -134,10 +134,10 @@ const Drawer = ({ toggleDrawer, isOpen, algo }) => {
 
     return (
         <>
-            <Background onClick={toggleDrawer} isOpen={isOpen} />
-            <DrawArea isOpen={isOpen}>
-                <DrawerToggle isOpen={isOpen} onClick={toggleDrawer}>
-                    <DrawerIcon isOpen={isOpen} />
+            <Background onClick={toggleDrawer} $isOpen={isOpen} />
+            <DrawArea $isOpen={isOpen}>
+                <DrawerToggle $isOpen={isOpen} onClick={toggleDrawer}>
+                    <DrawerIcon $isOpen={isOpen} />
                 </DrawerToggle>
                 <Tip>
                     <BsFillInfoCircleFill style={{ marginRight: "0.5rem" }} />
